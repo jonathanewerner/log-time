f36bb74244e360185e9af69d7bfaab139db91879
Jonathan Werner <jwerner@jona.local>
2016-07-03 14:25:07 +0200

    init

---
+++ README.md
@@ -0,0 +1,58 @@
+# log-time
+
+Easily log time spent on your notebook, distinguish between personal and work hours.
+
+Premises: 
+- I *don't* need to track projects
+- I just want to track personal and working time slots. Automatically. 
+- The time tracking should auto-stop/resume on notebook standby/wake. 
+- It should *not* autopause on inactivity as i'm often not on my notebook while pair-programming.
+
+### Setup
+Basic idea: another tool (for example Keyboard Maestro for MacOS) triggers a bash script on notebook standby and wake up, plus provides two key bindings for triggering the same script, setting the "work mode" ("personal" or "work").
+The script looks like this:
+
+```bash
+# file: log-time-add-entry (executable on your PATH)
+DATE=$(date +"%Y-%m-%dT%H:%M:%S+0100")
+echo "{\"date\": \"${DATE}\", \"action\": \"$1\"}" >> ~/.time/log.txt
+(cd ~/.time && git commit -am "${DATE}")
+```
+
+The script depends on the folder `~/.time` and a initialized git repo in that folder to backup each log entry.
+
+- The script call for the sleep event looks like `log-time-add-entry sleep`
+- The script call for the wake event looks like `log-time-add-entry wake`
+- The script call for keyboard shortcut to set the work mode to "personal" looks like `log-time-add-entry personal`
+- The script call for keyboard shortcut to set the work mode to "work" looks like `log-time-add-entry work`
+
+Calling the script appends a JSON-object with the current `date` and the `action` supplied via cli arg `$1`.
+
+`~/time/log.txt` will look like:
+
+```
+...
+{"date": "2016-07-03T13:39:00+0100", "action": "wake"}
+{"date": "2016-07-03T13:47:06+0100", "action": "work"}
+{"date": "2016-07-03T13:50:41+0100", "action": "personal"}
+{"date": "2016-07-03T13:51:20+0100", "action": "sleep"}
+{"date": "2016-07-03T13:51:25+0100", "action": "wake"}
+{"date": "2016-07-03T14:02:35+0100", "action": "work"}
+{"date": "2016-07-03T14:02:51+0100", "action": "personal"}
+{"date": "2016-07-03T14:04:29+0100", "action": "personal"}
+{"date": "2016-07-03T14:04:36+0100", "action": "work"}
+{"date": "2016-07-03T14:15:28+0100", "action": "personal"}
+{"date": "2016-07-03T14:18:16+0100", "action": "personal"}
+{"date": "2016-07-03T14:18:23+0100", "action": "work"}
+{"date": "2016-07-03T14:20:34+0100", "action": "personal"}
+{"date": "2016-07-03T14:23:11+0100", "action": "sleep"}
+{"date": "2016-07-03T14:23:17+0100", "action": "wake"}
+...
+```
+
+The advantage of this file based format is that it's 
+- trivial to manually correct when something went wrong 
+- automatically backup-ed via git.
+
+### Run
+Calling `node index.js` (feel free to `ln -s` this into a nice script name in your path) from this repo should give you a breakdown per week of your personal and work-related hours:
---
+++ index.js
@@ -0,0 +1,121 @@
+#!/usr/bin/env node
+/* eslint-disable */
+'use strict'
+const cat = require('shelljs').cat
+const findIndex = require('lodash/findIndex')
+const noop = require('lodash/noop')
+const transform = require('lodash/transform')
+const groupBy = require('lodash/groupBy')
+const sortBy = require('lodash/sortBy')
+const sumBy = require('lodash/sumBy')
+const invariant = require('invariant')
+const moment = require('moment')
+const chalk = require('chalk')
+const humanizeDuration = require('humanize-duration')
+
+Date.prototype.addHours = function(h) {
+	this.setHours(this.getHours() + h);
+	return this;
+}
+
+const WAKE = 'wake'
+const SLEEP = 'sleep'
+const WORK = 'work'
+const PERSONAL = 'personal'
+
+const file = cat('~/.time/log.txt')
+const lines = file.split('\n').filter((line) => line)
+
+/* eslint-disable indent */
+const entries = lines.map((line) => {
+	const parsed = JSON.parse(line)
+	return { date: new Date(parsed.date), action: parsed.action }
+})
+/* eslint-enable indent */
+
+const firstWakeIndex = findIndex(entries, (entry) => entry.action === WAKE)
+const firstSleepIndex = findIndex(entries, (entry) => entry.action === SLEEP)
+invariant(firstSleepIndex === -1 || firstWakeIndex < firstSleepIndex, 'The first "wake"" entry should come before the first "sleep" entry.')
+
+function getTimeSlots(entries) {
+	let working = true
+	let currentSlot
+	const endSlot = (slot, entry) => {
+		slot.end = entry.date
+		slot.duration = Math.floor((slot.end.getTime() - slot.start.getTime()) / 1000)
+		invariant(slot.duration > 0, `slot.duration is negative for slot ${JSON.stringify(slot, null, 2)}`)
+	}
+	const makeNewSlot = (entry) => ({start: entry.date, type: working ? 'work' : 'personal'})
+	const result = transform(entries, (slots, entry) => {
+		switch (entry.action) {
+			case WAKE:
+				currentSlot = makeNewSlot(entry)
+			break
+			case SLEEP: {
+				if (!currentSlot) return
+				invariant(currentSlot, 'SlEEP action executed without a current slot.')
+				endSlot(currentSlot, entry)
+				slots.push(currentSlot)
+				currentSlot = null
+			}
+			break
+			case PERSONAL: {
+				if (!working) return
+				// invariant(working, `PERSONAL action executed while !working, looking at ${JSON.stringify(entry)}`)
+				working = false
+				endSlot(currentSlot, entry)
+				slots.push(currentSlot)
+				currentSlot = makeNewSlot(entry)
+			}
+			break
+			case WORK: {
+				if (working) return
+				// invariant(!working, `WORK action executed while working, looking at ${JSON.stringify(entry)}`)
+				working = true
+				endSlot(currentSlot, entry)
+				slots.push(currentSlot)
+				currentSlot = makeNewSlot(entry)
+			}
+			break
+		}
+	}, [])
+	if (currentSlot) {
+		endSlot(currentSlot, {date: new Date().addHours(1)})
+		currentSlot.current = true
+    result.push(currentSlot)
+ }
+	return result
+}
+
+const durationPretty = (seconds) => humanizeDuration(seconds * 1000)
+const time = (date) => moment(date).utcOffset(1).format('h:mma')
+const date = (date) => moment(date).utcOffset(1).format('ddd, D.M')
+
+function printTimeSlots(timeSlots) {
+  timeSlots.forEach((slot) => {
+		const s = `${slot.current ? 'Current' : date(slot.start)}: ${time(slot.start)}—${time(slot.end)} (${durationPretty(slot.duration)})`
+		const style = slot.type === PERSONAL
+			? (str) => chalk.dim(str)
+			: (str) => chalk.black(str)
+		console.log(style(s))
+  })
+}
+
+function printTimeSlotsByYearWeek(timeSlotsByYearWeek) {
+	sortBy(Object.keys(timeSlotsByYearWeek)).forEach((yearWeek) => {
+		const timeSlotsForYearWeek = timeSlotsByYearWeek[yearWeek]
+		console.info(chalk.yellow(yearWeek))
+		printTimeSlots(timeSlotsForYearWeek)
+		const overallTimeSpentWorking = sumBy(timeSlotsForYearWeek.filter((slot) => slot.type === WORK), (slot) => slot.duration)
+		const overallTimeSpentPersonal = sumBy(timeSlotsForYearWeek.filter((slot) => slot.type === PERSONAL), (slot) => slot.duration)
+		console.info()
+		console.info(`Working: ${durationPretty(overallTimeSpentWorking)}`)
+		overallTimeSpentPersonal && console.info(chalk.dim(`Personal: ${durationPretty(overallTimeSpentPersonal)}`))
+		console.info()
+ })
+}
+
+const slots = getTimeSlots(entries);
+const groupedByYearWeek = groupBy(slots, (slot) => `${moment(slot.start).year()}-${moment(slot.start).week()}`)
+
+printTimeSlotsByYearWeek(groupedByYearWeek)
